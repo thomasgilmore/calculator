87324abaa1fcce023f4ff80c29e6594c891b66a8
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-04-01 19:02:11 -0400

    created add, subtract, multiply, and divide functions

---
+++ script.js
@@ -0,0 +1,23 @@
+let number1 = 5;
+let number2 = 5;
+
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+} 
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+// console.log(`add: ${add(number1, number2)}
+// subtract: ${subtract(number1, number2)}
+// multiply: ${multiply(number1, number2)}
+// divide: ${divide(number1, number2)}`)
