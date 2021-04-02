f2c6015c0c74f3c5b039492f0ec12e2d2bff08d9
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-04-01 21:51:19 -0400

    working on calculator

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-let number1 = 5;
-let number2 = 5;
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const divideButton = document.getElementById("divideButton");
+
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const multiplyButton = document.getElementById("multiplyButton");
+
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const subtractButton = document.getElementById("subtractButton");
+
+const clearButton = document.getElementById("clearButton");
+const zero = document.getElementById("zero");
+const equalButton = document.getElementById("equalButton");
+const addButton = document.getElementById("addButton");
+
+let number1;
+let number2;
+let answer;
+let operatorButton;
+
+let displayNumber = '';
 
 function add(number1, number2) {
     return number1 + number2;
@@ -20,6 +44,57 @@ function divide(number1, number2) {
 function operate(number1, operator, number2) {
    return operator(number1, number2);
 }
+
+function populateTheDisplay(newButtonClicked) {
+    if (newButtonClicked === '1' || newButtonClicked === '2' || newButtonClicked === '3' || newButtonClicked === '4' || newButtonClicked === '5' || newButtonClicked === '6' || newButtonClicked ===  '7' || newButtonClicked === '8' || newButtonClicked === '9' || newButtonClicked === '0') {
+        displayNumber = displayNumber + newButtonClicked;
+        document.getElementById("display").innerHTML= displayNumber;
+    } else if (newButtonClicked === 'clear') {
+        displayNumber = '';
+        number1 = '';
+        number2 = '';
+        operatorButton = '';
+        document.getElementById("display").innerHTML= displayNumber;
+    } else if (newButtonClicked === 'addButton' || newButtonClicked === 'subtractButton' || newButtonClicked === 'multiplyButton' || newButtonClicked === 'divideButton') {
+        number1 = displayNumber;
+        operatorButton = newButtonClicked;
+        displayNumber = '';
+        console.log(number1);
+        console.log(operatorButton);
+        console.log(displayNumber);
+        document.getElementById("display").innerHTML= displayNumber;
+    } else if (newButtonClicked === 'equalButton') {
+        if (operatorButton === 'addButton') {
+            answer = add(number1, displayNumber);
+            console.log(answer);
+            document.getElementById("display").innerHTML= answer;
+        }
+    }
+    
+}
+
+seven.addEventListener('click', function() { populateTheDisplay('7') });
+eight.addEventListener('click', function() { populateTheDisplay('8') });
+nine.addEventListener('click', function() { populateTheDisplay('9') });
+divideButton.addEventListener('click', function() { populateTheDisplay('divideButton') });
+
+
+four.addEventListener('click', function() { populateTheDisplay('4') });
+five.addEventListener('click', function() { populateTheDisplay('5') });
+six.addEventListener('click', function() { populateTheDisplay('6') });
+multiplyButton.addEventListener('click', function() { populateTheDisplay('multiplyButton') });
+
+
+one.addEventListener('click', function() { populateTheDisplay('1') });
+two.addEventListener('click', function() { populateTheDisplay('2') });
+three.addEventListener('click', function() { populateTheDisplay('3') });
+subtractButton.addEventListener('click', function() { populateTheDisplay('subtractButton') });
+
+clearButton.addEventListener('click', function() { populateTheDisplay('clear') });
+zero.addEventListener('click', function() { populateTheDisplay('0') });
+equalButton.addEventListener('click', function() { populateTheDisplay('equalButton') });
+addButton.addEventListener('click', function() { populateTheDisplay('addButton') });
+
 
 // console.log(operate(5, add, 15));
 
